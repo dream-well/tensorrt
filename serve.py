a60380b03d852d6aff797c8980737597202b1375
dream-well <dreamwell506@outlook.com>
2024-09-05 14:34:00 -0400

    Update serve.py

--- serve.py
+++ serve.py
@@ -44,7 +44,8 @@ def build_and_run_llama(hf_model_dir, engine_dir, force_build, tp_size, rank):
     build_config = BuildConfig(max_input_len=128,
                                max_seq_len=512,
                                opt_batch_size=8,
-                               max_batch_size=8)
+                               max_num_tokens=4096,
+                               max_batch_size=16)
     # build_config.builder_opt = 0  # fast build for demo, pls avoid using this in production, since inference might be slower
     build_config.plugin_config.gemm_plugin = 'bfloat16'  # for fast build, tune inference perf based on your needs
     build_config.plugin_config.gpt_attention_plugin = 'bfloat16'  # for fast build, tune inference perf based on your needs
