1dba106cf9b221bb97a57638b5355563edf12831
dream-well <dreamwell506@outlook.com>
2024-10-24 09:52:28 +0000

    serve models

--- serve_models.sh
+++ serve_models.sh
@@ -1,5 +1,5 @@
-# pm2 start start.sh --name s9001 -- NousResearch/Meta-Llama-3.1-8B-Instruct 9000 &
-# pm2 start start.sh --name s9002 -- NousResearch/Hermes-3-Llama-3.1-8B 9000 &
-# pm2 start start.sh --name s9003 -- NTQAI/Nxcode-CQ-7B-orpo 9000 &
-# pm2 start start.sh --name s9004 -- gryphe/mythomax-l2-13b 9000 &
-# pm2 start start.sh --name s9005 -- deepseek-ai/deepseek-coder-33b-instruct 9000 &
+# pm2 start start.sh --name server -- NousResearch/Meta-Llama-3.1-8B-Instruct 8000 &
+# pm2 start start.sh --name server -- NousResearch/Hermes-3-Llama-3.1-8B 8000 &
+# pm2 start start.sh --name server -- NTQAI/Nxcode-CQ-7B-orpo 8000 &
+# pm2 start start.sh --name server -- gryphe/mythomax-l2-13b 8000 &
+# pm2 start start.sh --name server -- deepseek-ai/deepseek-coder-33b-instruct 8000 &
