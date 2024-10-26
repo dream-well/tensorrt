1c67f977701a244b4961ca6124f479a668672d10
dream-well <dreamwell506@outlook.com>
2024-10-26 12:02:35 -0400

    Update serve_vllm.py

--- serve_vllm.py
+++ serve_vllm.py
@@ -99,9 +99,7 @@ class LLMGenerator:
                 first_at = time.time()
             token_id = output.token_ids[-1]
             logprob = output.logprobs[-1].get(token_id)
-            response = [token_id, logprob.logprob if logprob is not None else 1e-8]
-            if model_id == 3:
-                response.append(self.TOKENIZER.decode([token_id]))
+            response = [token_id, logprob.logprob if logprob is not None else 1e-8, self.TOKENIZER.decode([token_id])]
             responses.append(response)
             if len(responses) > 30:
                 yield json.dumps(responses)
