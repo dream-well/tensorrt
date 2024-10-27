704c11f92fa4160c8bcc03d06bba3069a47821bd
dream-well <dreamwell506@outlook.com>
2024-10-26 23:25:19 -0400

    Create health.py

---
+++ health.py
@@ -0,0 +1,68 @@
+import asyncio
+import json
+import httpx
+import requests
+import time
+import os
+
+request = {
+    "request_type":'COMPLETION',
+    "model":'NousResearch/Meta-Llama-3.1-8B-Instruct',
+    "temperature":1,
+    "seed": 1,
+    "max_tokens":1,
+    "prompt": """Hello"""
+}
+
+output_sequence = [
+    {"text":'1', "logprob":0.0, "powv":4616926, "token_id":16}]
+
+print("===== response text =====")
+
+url = 'http://localhost:8000/generate_async'
+
+failed_count = 0
+
+async def get_request():
+    """Forward the incoming request to a backend server and stream the response."""
+    start_at = time.time()
+
+    async with httpx.AsyncClient() as client:
+        # Stream the request to the backend server
+        try:
+            async with client.stream(
+                'POST',
+                url=url,
+                content=json.dumps(request),
+                headers={'model': request['model']},
+                follow_redirects=True,
+                timeout=10
+            ) as response_stream:
+                
+                # Stream the response from the backend
+                async def iter_response():
+                    outputs = []
+                    async for chunk in response_stream.aiter_bytes():
+                        print(chunk)
+                        outputs.extend(json.loads(chunk))
+                    duration = time.time() - start_at
+                    print(f"{(len(outputs) / duration):.2f} tps, {len(outputs)} tokens in {duration:.2f} seconds")
+                
+                await iter_response()
+            failed_count = 0
+        except Exception as e:
+            print(e)
+            failed_count += 1
+            if failed_count > 3:
+                print("Failed too many times, exiting")
+            os.system("pm2 restart serve_vllm")
+            time.sleep(30)
+    requests.post(url, json=request)
+
+
+# count = 1000
+while True:
+    asyncio.run(get_request())
+    time.sleep(20)
+
+        
